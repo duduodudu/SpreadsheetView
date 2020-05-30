35236649be5f80d4d68e6ded5f9f879ddb417b5e
duduodudu <duweiquan@foxmail.com>
2020-05-30 11:37:22 +0800

    swift 5
    swift 升级到5.0

--- Framework/Tests/HostApp/AppDelegate.swift
+++ Framework/Tests/HostApp/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    internal func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
 }
